3ab53ded65f1f2bbe56772ec3b93db634e8811f0
Ariel <el.ariel.cl@gmail.com>
2015-12-22 14:25:45 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='prueba',
+      version='0.1.1',
+      description='Test',
+      url='http://www.onlinereyes.com',
+      author='Ariel',
+      author_email='el.ariel.cl@gmail.com',
+      license='MIT',
+      packages=['prueba'],
+      zip_safe=False)
